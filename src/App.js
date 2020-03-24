7a58d7ed8605c582c9cc4e8c12ad579caabe1741
donsimon <speak2donsimon@gmail.com>
2020-03-24 06:00:15 +0100

    portfolio initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route as RouteLink } from "react-router-dom";
-import Sidebar from "./components/Sidebar/sidebar";
-import About from "./components/Pages/about";
-import Portfolio  from "./components/Pages/portfolio";
+import { makeStyles, Container, Divider } from "@material-ui/core";
+import NavBar from "./components/navbar";
+import About from "./components/pages/about";
+import Education from "./components/pages/about";
+import Experience from "./components/pages/about";
+
 import "./App.css";
 
+// =============================================================================
+// FontAwesomeIcons
+// =============================================================================
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+
+const useStyles = makeStyles(() => ({
+  dark: {
+    background: "#242433",
+    color: "white"
+  }
+}));
+
 function App(props) {
+  const [state, setState] = useState(false);
+  const style = useStyles();
+  const body = document.querySelector("body");
+  // const d = new Date();
+  // const hour = d.getHours();
+  // const night = hour >= 19 || hour <= 7;
+  const classes =
+    state === false
+      ? body.classList.remove(`${style.dark}`)
+      : body.classList.add(`${style.dark}`);
+  const toggle = () => {
+    if (state === false) {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  };
+  // const Layout = {
+  //   maxWidth: "100%",
+  //   maxHeight: "100vh",
+  //   Height: "100%"
+  // };
   return (
-    <Router>
-      <div className='main'>
-        <div>
-          <Sidebar />
-          <div className='pages'>
-            <RouteLink exact path='/' component={About} />
-            <RouteLink path='/portfolio' component={Portfolio} />
-          </div>
-        </div>
-      </div>
-    </Router>
+    <Container maxWidth={"lg"} className='App'>
+      <NavBar dark={classes} toggle={toggle} />
+      <About />
+      <Divider/>
+      <Education />
+      <Divider/>
+      <Experience />
+    </Container>
   );
 }
 
